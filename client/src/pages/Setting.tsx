5e458c3e489c6124bc67ad41cce7e2866f533bd8
unknown <bintangkusumam@gmail.com>
2025-10-15 16:39:09 +0700

    new

--- client/src/pages/Setting.tsx
+++ client/src/pages/Setting.tsx
@@ -2,27 +2,26 @@ import DataModifier from "../services/data-modifier";
 import useAuth from "../services/useAuth";
 import type { IUserInfo } from "../services/custom-types";
 import { Navbar1, Navbar2 } from "../components/Navbar";
-import useSWR from "swr";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function Setting() {
     const { user } = useAuth();
     const { deleteData, getData } = DataModifier();
-    
-    const { data: userData } = useSWR<IUserInfo[]>(
-        user ? `http://localhost:1234/users/selected/${user.info.id}` : '',
-        getData,
-        {
-            revalidateOnFocus: true,
-            revalidateOnReconnect: true,
-            dedupingInterval: 5000,
-            errorRetryCount: 3
-        }
-    );
+    const queryQlient = useQueryClient();
 
-    const deleteAllSignedUserPosts = async () => {
-        if (!user || !window.confirm('Are you sure you want to delete this post?')) return;
-        await deleteData(`http://localhost:1234/posts/erase-all/${user.info.id}`);
-    }
+    const { data: userData } =  getData<IUserInfo[]>(user ? `http://localhost:1234/users/selected/${user.info.id}` : '', ['signed-in-user']);
+
+    const deleteAllPostMutation = useMutation({
+        mutationFn: async () => {
+            if (!user || !window.confirm('Are you sure you want to delete this post?')) return;
+            await deleteData(`http://localhost:1234/posts/erase-all/${user.info.id}`);
+        }, onSuccess: () => {
+            queryQlient.invalidateQueries({ queryKey: ['all-posts'] });
+            queryQlient.invalidateQueries({ queryKey: ['signed-user-posts'] });
+        }
+    });
+
+    const deleteAllSignedUserPosts = () => deleteAllPostMutation.mutate();
 
     return (
         <div className="h-screen flex md:flex-row flex-col gap-[1rem] p-[1rem] bg-black">
@@ -30,10 +29,9 @@ export default function Setting() {
             <Navbar2/>
             <div className="flex justify-center p-[1rem] gap-[1rem] bg-[#1a1a1a] w-full md:w-3/4 h-full">
                 <div className="flex flex-col gap-[1rem]">
-                    <p className="text-purple-400 font-[500] text-[1rem]">Email: {userData && userData[0].email}</p>
-                    <p className="text-purple-400 font-[500] text-[1rem]">Username: {userData && userData[0].username}</p>
+                    <p className="text-purple-400 font-[500] text-[1rem]">Email: {userData ? userData.data[0].email : ''}</p>
+                    <p className="text-purple-400 font-[500] text-[1rem]">Username: {userData ? userData.data[0].username : ''}</p>
                     <div className="flex gap-[0.4rem]">
-                    
                         <button 
                             type="button"
                             onClick={deleteAllSignedUserPosts}
