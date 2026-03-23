2c40ce9296c32666f49733c68f1c0cebfc062a42
unknown <bintangkusumam@gmail.com>
2026-03-23 23:05:49 +0700

    >_<

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useCallback } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../services/auth.service";
+import { Eye, EyeOff } from "lucide-react";
 
 export default function SignIn() {
     const { error, loading, setError, user, signIn } = useAuth();
@@ -8,6 +9,7 @@ export default function SignIn() {
 
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [showPassword, setShowPassword] = useState<boolean>(false);
 
     useEffect(() => {
         if (user) navigate('/home', { replace: true });
@@ -20,49 +22,59 @@ export default function SignIn() {
         }
     }, [error]);
 
+    const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
     const handleSignIn = useCallback(async (event: React.FormEvent): Promise<void> => {
         event.preventDefault();
         await signIn(email.trim(), password.trim());
     }, [email, password]);
 
     return (
-        <div className="flex justify-center items-center h-screen bg-[#1a1a1a]">
-            <form onSubmit={handleSignIn} className="border bg-black border-purple-400 p-[1rem] flex flex-col gap-[1rem] w-[320px]">
+        <div className="flex justify-center items-center h-screen bg-[#1a1a1a] p-3">
+            <form onSubmit={handleSignIn} className="border bg-black border-purple-400 p-[1rem] flex flex-col gap-[1rem] w-120">
                 <div className="font-[650] text-[1.5rem] text-center text-purple-400">Hello</div>
                 
                 <div className="flex flex-col gap-[0.5rem]">
-                    <label htmlFor="email" className="text-purple-400 font-[600]">Email</label>
+                    <label htmlFor="email" className="text-purple-400 font-medium">Email</label>
                     <input 
                         type="email" 
                         id="email" 
                         value={email}
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
-                        className="p-[0.45rem] text-[0.9rem] text-purple-400 outline-0 border border-purple-400 font-[600] rounded" 
+                        className="p-3 w-full text-purple-400 outline-0 border border-purple-400 font-medium rounded" 
                         placeholder="your@gmail.com"
                     />
                 </div>
                 
-                <div className="flex flex-col gap-[0.5rem]">
-                    <label htmlFor="password" className="font-[600] text-purple-400">Password</label>
+                <div className="flex flex-col gap-[0.5rem] relative">
+                    <label htmlFor="password" className="font-medium text-purple-400">Password</label>
                     <input 
                         type="password" 
                         id="password" 
                         value={password}
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
-                        className="p-[0.45rem] text-[0.9rem] text-purple-400 outline-0 border border-purple-400 font-[600] rounded" 
+                        className="w-full p-3 text-purple-400 outline-0 border border-purple-400 font-medium rounded pr-10" 
                         placeholder="your_password"
                     />
+                    <button
+                        type="button"
+                        onClick={togglePasswordVisibility}
+                        className="absolute top-8 cursor-pointer inset-y-0 right-0 flex items-center px-3 text-purple-200 hover:text-purple-400"
+                        aria-label={showPassword ? "Sembunyikan password" : "Tampilkan password"}
+                    >
+                        {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                    </button>
                 </div>
                 
                 <div className="text-center text-sm">
-                    <span className="text-white">Don't have an account?</span> <Link className="text-blue-700 hover:underline" to={'/signup'}>Sign Up</Link>
+                    <span className="text-white">Don't have an account?</span> <Link className="text-blue-400 hover:underline" to={'/signup'}>Sign Up</Link>
                 </div>
                 
-                {error ? <div className="text-blue-300 font-medium text-center text-[0.9rem]">{error}</div> : null}
+                {error ? <div className="text-blue-300 font-medium text-center text-base md:text-md">{error}</div> : null}
                 
                 <button 
                     type="submit" 
-                    className="p-[0.45rem] text-[0.9rem] outline-0 border-0 bg-purple-700 text-white font-[550] cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed rounded hover:bg-purple-800 transition-colors"
+                    className="p-[0.45rem] text-base md:text-md outline-0 border-0 bg-purple-700 text-white font-[550] cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed rounded hover:bg-purple-800 transition-colors"
                 >
                     {loading ? 'Signing In...' : 'Sign In'}
                 </button>
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../services/auth.service";
+import { Eye, EyeOff } from "lucide-react";
 
 export default function SignUp() {
     const { error, loading, setError, signUp, user } = useAuth();
@@ -9,6 +10,7 @@ export default function SignUp() {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [username, setUsername] = useState<string>('');
+    const [showPassword, setShowPassword] = useState<boolean>(false);
 
     useEffect(() => {
         if (user) navigate('/home', { replace: true });
@@ -21,14 +23,16 @@ export default function SignUp() {
         }
     }, [error]);
 
+    const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
     const hansleSignUp = useCallback(async (event: React.FormEvent): Promise<void> => {
         event.preventDefault();
         await signUp(new Date().toISOString(), email.trim(), username.trim(), password.trim());
     }, [email, password, username]);
 
     return (
-        <div className="flex justify-center items-center h-screen bg-[#1a1a1a]">
-            <form onSubmit={hansleSignUp} className="border border-purple-400 p-[1rem] flex flex-col gap-[1rem] bg-black w-[320px]">
+        <div className="flex justify-center items-center h-screen bg-[#1a1a1a] p-3">
+            <form onSubmit={hansleSignUp} className="border border-purple-400 p-[1rem] flex flex-col gap-[1rem] bg-black w-120">
                 <div className="font-[650] text-[1.5rem] text-purple-400 text-center">Welcome</div>
                 <div className="flex flex-col gap-[0.5rem]">
                     <label htmlFor="username" className="text-purple-400">Username</label>
@@ -37,7 +41,7 @@ export default function SignUp() {
                         id="username" 
                         value={username}
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUsername(event.target.value)}
-                        className="p-[0.45rem] text-[0.9rem] outline-0 border border-purple-400 text-purple-400 font-[600]" 
+                        className="w-full p-3 outline-0 border border-purple-400 text-purple-400 font-medium" 
                         placeholder="ex: john"
                     />
                 </div>
@@ -48,27 +52,35 @@ export default function SignUp() {
                         id="email" 
                         value={email}
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
-                        className="p-[0.45rem] text-[0.9rem] outline-0 border border-purple-400 text-purple-400 font-[600]" 
+                        className="w-full p-3 outline-0 border border-purple-400 text-purple-400 font-medium pr-10" 
                         placeholder="your@gmail.com"
                     />
                 </div>
-                <div className="flex flex-col gap-[0.5rem]">
+                <div className="flex flex-col gap-[0.5rem] relative">
                     <label htmlFor="password" className="text-purple-400">Password</label>
                     <input 
-                        type="password" 
+                        type={showPassword ? "text" : "password"} 
                         id="password" 
                         value={password}
                         onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)}
-                        className="p-[0.45rem] text-[0.9rem] outline-0 border border-purple-400 text-purple-400 font-[600]" 
+                        className="w-full p-3 outline-0 border border-purple-400 text-purple-400 font-medium" 
                         placeholder="your_password"
                     />
+                    <button
+                        type="button"
+                        onClick={togglePasswordVisibility}
+                        className="absolute top-8 cursor-pointer inset-y-0 right-0 flex items-center px-3 text-purple-200 hover:text-purple-400"
+                        aria-label={showPassword ? "Sembunyikan password" : "Tampilkan password"}
+                    >
+                        {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                    </button>
                 </div>
-                <div className="text-center text-white">Already have account? <Link className="text-blue-700" to={'/signin'}>SignIn</Link></div>                    
-                {error ? <div className="text-blue-300 font-medium text-[0.9rem] text-center">{error}</div> : null}
+                <div className="text-center text-white">Already have account? <Link className="text-blue-400" to={'/signin'}>SignIn</Link></div>                    
+                {error ? <div className="text-blue-300 font-medium text-base md:text-md text-center">{error}</div> : null}
                 <button 
                     type="submit" 
                     disabled={loading}
-                    className="p-[0.45rem] text-[0.9rem] outline-0 border-0 bg-purple-700 text-white font-[550] cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+                    className="p-[0.45rem] text-base md:text-md outline-0 border-0 bg-purple-700 text-white font-[550] cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
                 >
                     Sign Up
                 </button>
