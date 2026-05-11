afcf1c308793bef54fd9556ca815b5be6bf731e6
unknown <bintangkusumam@gmail.com>
2026-05-11 13:00:09 +0700

    >_<

--- client/src/components/SearchedPostList.tsx
+++ client/src/components/SearchedPostList.tsx
@@ -1,4 +1,4 @@
-import type { PostListProps } from "../services/custom-types";
+import type { PostListProps } from "../models/post-model";
 import Loading from "./Loading";
 import PostItem from "./PostItem";
 
